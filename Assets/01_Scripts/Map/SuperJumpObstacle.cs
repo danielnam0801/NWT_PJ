0095b990b98b50be2729a56d3f2563801b613311
JUNSUNG06 <forest2847@gmail.com>
2023-04-26 19:05:30 +0900

    .

--- Assets/01_Scripts/Map/SuperJumpObstacle.cs
+++ Assets/01_Scripts/Map/SuperJumpObstacle.cs
@@ -5,23 +5,16 @@ using UnityEngine;
 
 public class SuperJumpObstacle : MonoBehaviour
 {
-    private Rigidbody2D _rigid;
     [SerializeField] LayerMask _layer;
     [SerializeField] private float _jumpPower;
-    [SerializeField] private ParticleSystem _particlePrefab;
-
-    private void Awake()
-    {
-        _rigid = GameObject.Find("Player").GetComponent<Rigidbody2D>();
-    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
-        { 
-            _rigid.AddForce(Vector2.up * _jumpPower, ForceMode2D.Impulse);
+        {
+            Debug.Log(collision.gameObject.GetComponent<Rigidbody2D>());
 
-            PoolManager.Instance.Pop(_particlePrefab.name);
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * _jumpPower, ForceMode2D.Impulse);
         }
     }
 }
