f6d856f86b57bc6875aa2b56afe75ad01a14f755
JUNSUNG06 <forest2847@gmail.com>
2023-05-12 22:48:10 +0900

    .

--- Assets/UIToolkit/UI/InGameUI/InGameUIController.cs
+++ Assets/UIToolkit/UI/InGameUI/InGameUIController.cs
@@ -17,43 +17,11 @@ public class InGameUIController : MonoBehaviour
             settingUI.SetActive(true);
         });
 
-        Slider slider = root.Q<Slider>("Slider");
-        slider = GetComponentInChildren<Slider>();
+        //Slider slider = root.Q<Slider>("Slider");
+        //slider = GetComponentInChildren<Slider>();
+
+        VisualElement leftBtn = root.Q<VisualElement>("LeftBtn");
+        leftBtn.AddManipulator(new ClickManipulator(() => Debug.Log(1), () => Debug.Log(2)));
 
     }
-    //private UIDocument _doc;  // 요 스크립트와 같은 게임 오브젝트에 있는 UI Document 컴포넌트 할당용
-
-    //private Button _leftBtn;
-    //private Button _rightBtn;
-    //private Button _settingBtn;
-    //private Button _skill1Btn;
-    //private Button _skill2Btn;
-    //private Button _skill3Btn;
-    //private Button _skill4Btn;
-    //private Button _skill5Btn;
-    //private void Awake()
-    //{
-    //    _doc = GetComponent<UIDocument>();
-
-    //    // 각 버튼의 가져옴
-    //    _leftBtn = _doc.rootVisualElement.Q<Button>("LeftBtn");
-    //    _rightBtn = _doc.rootVisualElement.Q<Button>("RightBtn");
-    //    _skill1Btn = _doc.rootVisualElement.Q<Button>("");
-    //    _skill2Btn = _doc.rootVisualElement.Q<Button>("");
-    //    _skill3Btn = _doc.rootVisualElement.Q<Button>("");
-    //    _skill4Btn = _doc.rootVisualElement.Q<Button>("");
-    //    _skill5Btn = _doc.rootVisualElement.Q<Button>("");
-    //    _leftBtn.clicked += LeftBtnClick;
-    //    _rightBtn.clicked += RightBtnClick;
-    //}
-
-    //private void LeftBtnClick()
-    //{
-    //    Debug.Log("LeftButtonClicked");
-    //}
-    //private void RightBtnClick()
-    //{
-    //    Debug.Log("RightBtnClick");
-    //}
-
 }
